e07b691fb4fbb4c95e1225f844ce02fcf0153533
Kevin McClellan <kevin@ace4it.com>
2023-08-06 22:50:04 -0400

    tweak img srcset

--- config/shortcodes.js
+++ config/shortcodes.js
@@ -1,13 +1,13 @@
 const Image = require("@11ty/eleventy-img");
 
-const image = async (
+const carousel = async (
   src,
   alt,
   className = undefined,
   dataSrc = undefined,
-  widths = [300],
+  widths = [300, 1200],
   formats = ["webp", "jpeg"],
-  sizes = "100vw"
+  sizes = "(max-width: 1200px) 100vw, 1200px"
 ) => {
   const metadata = await Image(src, {
     widths: [...widths, null],
@@ -29,4 +29,33 @@ const image = async (
   return Image.generateHTML(metadata, attributes);
 };
 
-module.exports = { image };
+const lightbox = async (
+  src,
+  alt,
+  className = undefined,
+  dataSrc = undefined,
+  widths = [300, 500, 800],
+  formats = ["webp", "jpeg"],
+  sizes = "(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 300px"
+) => {
+  const metadata = await Image(src, {
+    widths: [...widths, null],
+    formats: [...formats, null],
+    outputDir: "_site/images",
+    urlPath: "/images",
+  });
+
+  const attributes = {
+    alt,
+    sizes,
+    "data-src": dataSrc,
+    loading: "lazy",
+    decoding: "async",
+  };
+  if (className !== undefined) {
+    attributes.class = className;
+  }
+  return Image.generateHTML(metadata, attributes);
+};
+
+module.exports = { carousel, lightbox };
